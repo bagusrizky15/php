48a9afee92741bb12c31e304df0d8f7d2fedf57f
mohamad.15@students.amikom.ac.id <mohamad.15@students.amikom.ac.id>
2022-06-05 20:55:41 +0700

    .

--- 01_array3.php
+++ 01_array3.php
@@ -1,21 +1,28 @@
 <?php
 
-$mahasiswa = ["Bagus", 2711 , "Informatika"];
+$mahasiswa = [["Bagus", 2711, "Informatika"], 
+["Rehan", 2121, "Sistem Informasi"]];
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <title>Daftar Mahasiswa</title>
 </head>
+
 <body>
     <h1>Data</h1>
+
+    <?php foreach($mahasiswa as $mhs) : ?>
     <ul>
-       <?php foreach($mahasiswa as $mhs) : ?>
-        <li><?= $mhs; ?></li>
-        <?php endforeach; ?>
+        <li>Nama : <?= $mhs[0]?></li>
+        <li>NIM : <?= $mhs[1]?></li>
+        <li>Jurusan : <?= $mhs[2]?></li>
     </ul>
-    
+    <?php endforeach; ?>
+
 </body>
+
 </html>
