9f3b11ec38c3883d20f58ca6def0f1fb75874768
mohamad.15@students.amikom.ac.id <mohamad.15@students.amikom.ac.id>
2022-06-06 15:43:02 +0700

    Update 02_array_databuku.php

--- 02_array_databuku.php
+++ 02_array_databuku.php
@@ -1,13 +1,45 @@
-<?php 
-
+<?php
+$buku = [
+    [
+        "judul" => "Atomics Habit",
+        "pengarang" => "joko sasongko",
+        "isbn" => "19112711",
+        "harga" => 89000,
+        "tahun" => 2021
+    ],
+    [
+        "judul" => "Text Mining",
+        "pengarang" => "suyanto",
+        "isbn" => "20199389",
+        "harga" => 99000,
+        "tahun" => 2019
+    ],
+    [
+        "judul" => "Fotografi",
+        "pengarang" => "sulaeman",
+        "isbn" => "1809220",
+        "harga" => 77000,
+        "tahun" => 2017
+    ]
+];
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <title>Data Buku Perpustakaan</title>
 </head>
+
 <body>
-    
+    <ul>
+        <?php foreach ($buku as $databuku) : ?>
+            <?php foreach ($databuku as $datalengkap) : ?>
+                <li><?= $datalengkap ?></li>
+            <?php endforeach; ?>
+        <?php endforeach; ?>
+    </ul>
+
 </body>
+
 </html>
