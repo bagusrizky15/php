12107389f5c7619510d70ab78f9c5ab0351d27db
mohamad.15@students.amikom.ac.id <mohamad.15@students.amikom.ac.id>
2022-06-12 11:23:39 +0700

    login

--- login/login.php
+++ login/login.php
@@ -1,3 +1,23 @@
+<?php
+
+//cek tombol submit
+if (isset($_POST["submit"])) {
+    if ($_POST["username"] == "admin" && $_POST["password"] == "123") {
+        header("Location: admin.php");
+        exit;
+    } else {
+        $error = true;
+    }
+}
+
+//cek username dan pw
+
+//jika benar ganti halaman
+
+//jika salah tidak ganti halaman
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,15 +26,26 @@
 </head>
 
 <body>
-    <ul>
+    <h1>Login</h1>
+    <?php
+    if (isset($error)) : ?>
+        <p style="color: red; font-style:italic;">Username / Password Salah</p>
+    <?php endif; ?>
 
+    <ul>
         <form action="" method="POST">
             <li>
                 <label for="username">Username</label>
                 <input type="text" name="username" id="username">
             </li>
+            <li>
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password">
+            </li>
+            <li>
+                <Button type="submit" name="submit">Login</Button>
+            </li>
         </form>
-
     </ul>
 </body>
 
