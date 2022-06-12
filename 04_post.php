7490f60d55fd5e5e717389b7a23a84d5b5bf661c
mohamad.15@students.amikom.ac.id <mohamad.15@students.amikom.ac.id>
2022-06-12 08:34:26 +0700

    post

--- 04_post.php
+++ 04_post.php
@@ -3,3 +3,17 @@
 
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>POST</title>
+</head>
+<body>
+    <form action="04_post2.php" method="post">
+        <input type="text" name="nama">
+        <br>
+        <button type="submit" name="submit">Kirim</button>
+    </form>
+</body>
+</html>
---
+++ 04_post2.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>POST</title>
+</head>
+<body>
+    <h1>Selamat datang, <?= $_POST["nama"]; ?></h1>
+    <a href="04_post.php">Kembali</a>
+</body>
+</html>
