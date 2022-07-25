a33e6fee62531627feed6dfc7f43e4bc446cfda2
mohamad.15@students.amikom.ac.id <mohamad.15@students.amikom.ac.id>
2022-07-25 10:15:34 +0700

    add looping from database

--- phpmysql/index.php
+++ phpmysql/index.php
@@ -1,31 +1,33 @@
 <?php
 
-    //koneksi
-    $conn = mysqli_connect("localhost", "root", "root", "phpdasar");
+//koneksi
+$conn = mysqli_connect("localhost", "root", "root", "phpdasar");
 
-    //ambil data dari tabel mahasiswa
-    $result = mysqli_query($conn, "SELECT * FROM mahasiswa");
+//ambil data dari tabel mahasiswa
+$result = mysqli_query($conn, "SELECT * FROM mahasiswa");
 
-    //ambil data mahasiswa dari object result (fetch)
-    //mysqli_fetch_row // mengembalikan array numeric
-    //mysqli_fetch_object // mengembalikan object result // tidak akan di pakai
-    //mysqli_fetch_array //mengembalikan array numeric dan assosiatif
-    //mysqli_fetch_assoc // mengembalikan array assosiatif
-    while ($ambil = mysqli_fetch_assoc($result)) {
-        # code...
-        var_dump($ambil);
-    }
-    
-    
+//ambil data mahasiswa dari object result (fetch)
+//mysqli_fetch_row // mengembalikan array numeric
+//mysqli_fetch_object // mengembalikan object result // tidak akan di pakai
+//mysqli_fetch_array //mengembalikan array numeric dan assosiatif
+//mysqli_fetch_assoc // mengembalikan array assosiatif
+
+// while ($ambil = mysqli_fetch_assoc($result)) {
+//     # code...
+//     var_dump($ambil);
+// }
+
+
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-   
-    <title>php mysql</title>
+    <title>phpmysql</title>
 </head>
+
 <body>
     <h1>Daftar Mahasiswa</h1>
 
@@ -41,19 +43,27 @@
             <th>Jurusan</th>
         </tr>
 
-        <tr>
-            <td>1</td>
-            <td>
-                <a href="#">Ubah</a> |
-                <a href="#">Hapus</a>
-            </td>
-            <td><img src="" alt=""></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-        </tr>
+        <?php
+        $i = 1;
+        while ($row = mysqli_fetch_assoc($result)) : ?>
+            <tr>
+                <td><?= $i ?></td>
+                <td>
+                    <a href="#">Ubah</a> |
+                    <a href="#">Hapus</a>
+                </td>
+                <td><img src="img/<?= $row["gambar"] ?>" alt=""></td>
+                <td><?= $row["nim"] ?></td>
+                <td><?= $row["nama"] ?></td>
+                <td><?= $row["email"] ?></td>
+                <td><?= $row["jurusan"] ?></td>
+            </tr>
+
+        <?php
+            $i++;
+        endwhile; ?>
 
     </table>
 </body>
+
 </html>
