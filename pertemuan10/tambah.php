ddb5dc9ad91eb2fe1166e1dc5358a7b0c54055ba
mohamad.15@students.amikom.ac.id <mohamad.15@students.amikom.ac.id>
2022-08-05 21:14:28 +0700

    add required

--- pertemuan10/tambah.php
+++ pertemuan10/tambah.php
@@ -39,24 +39,24 @@ if (isset($_POST["submit"])) {
         <ul>
             <li>
                 <label for="nama">Nama</label>
-                <input type="text" name="nama" id="nama">
+                <input type="text" name="nama" id="nama" required>
             </li>
             <li>
                 <label for="nim">NIM</label>
-                <input type="text" name="nim" id="nim">
+                <input type="text" name="nim" id="nim" required>
             </li>
 
             <li>
                 <label for="email">Email</label>
-                <input type="text" name="email" id="email">
+                <input type="text" name="email" id="email" required>
             </li>
             <li>
                 <label for="jurusan">Jurusan</label>
-                <input type="text" name="jurusan" id="jurusan">
+                <input type="text" name="jurusan" id="jurusan" required>
             </li>
             <li>
                 <label for="gambar">Gambar</label>
-                <input type="text" name="gambar" id="gambar">
+                <input type="text" name="gambar" id="gambar" required>
             </li>
             <li>
                 <button type="submit" name="submit">
