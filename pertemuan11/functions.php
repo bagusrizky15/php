9e3d6551a899f110bb0709a4b3e6aaafe875733b
mohamad.15@students.amikom.ac.id <mohamad.15@students.amikom.ac.id>
2022-08-06 23:47:35 +0700

    .

--- pertemuan11/functions.php
+++ pertemuan11/functions.php
@@ -34,7 +34,11 @@ function tambah($data){
 
 function hapus($id){
     global $conn;
-    mysqli_query($conn, "DELETE FROM mahasiswa WHERE id =$id");
+
+    $query = "DELETE FROM mahasiswa
+    WHERE id = $id";
+
+    mysqli_query($conn, $query);
     
     return mysqli_affected_rows($conn);
 }
