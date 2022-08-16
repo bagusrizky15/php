1ac14c8659863c2c4ddf468d56140abd451d0cf7
mohamad.15@students.amikom.ac.id <mohamad.15@students.amikom.ac.id>
2022-08-16 18:36:53 +0700

    Create login.php

---
+++ pertemuan14/login.php
@@ -0,0 +1,51 @@
+<?php
+
+require 'functions.php';
+    if (isset($_POST["login"])) {
+        $username = $_POST["username"];
+        $password = $_POST["password"];
+
+    $result = mysqli_query($conn, "SELECT * FROM user WHERE
+        username = '$username'");
+        
+        if (mysqli_num_rows($result)===1) {
+            $row = mysqli_fetch_assoc($result);
+            if (password_verify($password, $row["password"])){
+                header("Location: index.php");
+                exit;
+            }
+        }
+
+
+    }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <h1>Halaman Login</h1>
+    <form action="" method="POST">
+        <ul>
+            <li>
+                <label for="username">Username</label>
+                <input type="text" name="username" id="username">
+            </li>
+            <li>
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password">
+            </li>
+            <li>
+                <button type="submit" name="login">Login</button>
+            </li>
+        </ul>
+
+    </form>
+</body>
+</html>
