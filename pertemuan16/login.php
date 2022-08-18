a38903f6711d709a98ab1f98d2e74a35953f65e5
mohamad.15@students.amikom.ac.id <mohamad.15@students.amikom.ac.id>
2022-08-18 13:29:57 +0700

    Update login.php

--- pertemuan16/login.php
+++ pertemuan16/login.php
@@ -1,19 +1,27 @@
 <?php
 session_start();
-
+require 'functions.php';
 //cek cookie
 if (isset($_COOKIE['id']) && isset($_COOKIE['key'])) {
         $id = $_COOKIE['id'];   
         $key = $_COOKIE['key'];   
+    
+        //ambil username berdasarkan id
+        $result =mysqli_query($conn, "SELECT username FROM user WHERE id = $id");
+        $row = mysqli_fetch_assoc($result);
+
+        //cek cookie dan username
+        if ($key === hash('sha256', $row['username'])) {
+            $_SESSION['login'] = true;
+        }
     }
-}
 
 if (isset($_SESSION["login"])) {
     header("Location: index.php");
     exit;
 }
 
-require 'functions.php';
+
     if (isset($_POST["login"])) {
         $username = $_POST["username"];
         $password = $_POST["password"];
