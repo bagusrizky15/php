9f2844d1928d0df3b6cddcf336513f691a72b249
mohamad.15@students.amikom.ac.id <mohamad.15@students.amikom.ac.id>
2022-08-18 12:29:08 +0700

    session

---
+++ pertemuan15_session/halaman1.php
@@ -0,0 +1,8 @@
+<?php
+// Start the session
+session_start();
+
+$_SESSION["favcolor"] = "green";
+$_SESSION["favanimal"] = "cat";
+echo "Session variables are set.";
+?>
---
+++ pertemuan15_session/halaman2.php
@@ -0,0 +1,7 @@
+<?php
+session_start();
+
+// Echo session variables that were set on previous page
+echo "Favorite color is " . $_SESSION["favcolor"] . ".<br>";
+echo "Favorite animal is " . $_SESSION["favanimal"] . ".";
+?>
